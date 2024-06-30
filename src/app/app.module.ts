90e87815fae33a4ac58eae9bca11e2ef8c272b79
bhushanmarathe <bhushanmarathe12@gmail.com>
2024-06-30 16:38:24 -0400

    Nesting Components

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { TestComponent } from './components/test/test.component';
+
+// @NgModule({
+//   declarations: [TestComponent],
+//   imports: [BrowserModule],
+//   providers: [],
+//   bootstrap: [TestComponent],
+// })
+// export class AppModule {}
