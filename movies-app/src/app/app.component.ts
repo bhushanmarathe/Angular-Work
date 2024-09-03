091dd197d70327ca6e2e59e199f5526c187dde66
bhushanmarathe <bhushanmarathe12@gmail.com>
2024-09-02 22:49:48 -0400

    Added new Files

--- movies-app/src/app/app.component.ts
+++ movies-app/src/app/app.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 
+import { DetailsComponent } from './components/test/details/details.component';
+//import { DetailsMoviesComponent } from './components/test/details-movies/details-movies.component';
+
+//import { DetailsReviewsComponent } from './components/test/details-reviews/details-reviews.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TestComponent],
+  imports: [
+    RouterOutlet,
+    TestComponent,
+    DetailsComponent,
+    //DetailsMoviesComponent,
+    //DetailsReviewsComponent,
+  ],
 })
 export class AppComponent {
   title = 'movies-app';
